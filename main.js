723216c2b6b21383e415a71462ec95fdac5e2fc4
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-08-03 14:46:00 +0300

    first task - height, weight, front image

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+let serachButton = document.getElementById("searchButon");
+let input = document.getElementById("search");
+let results = document.getElementById("results")
+
+
 const searchPokemon = async (pokemonId = 3) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
+  let name = data.name;
+  let weight = data.weight;
+  let height = data.height;
+  let frontImg = data.sprites.front_default;
+  let pokeDiv = document.createElement("div")
+
+  pokeDiv.innerHTML = "<img src =" + frontImg + ">" + name + "'s Height is " + height + " and weight is " + weight
+  results.appendChild(pokeDiv)
+
+
 };
-searchPokemon();
+
+serachButton.addEventListener("click", () => searchPokemon(input.value))
 
