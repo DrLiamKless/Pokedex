8f03c657cae3f36d385c840b059f2e854cfc95f8
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-08-03 21:11:22 +0300

    adding list of pokemons with the same types when press

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // Defining global values
 let searchButton = document.getElementById("searchButon");
 let input = document.getElementById("search");
@@ -6,11 +5,10 @@ let results = document.getElementById("results");
 let pokeDiv = document.createElement("div");
 results.appendChild(pokeDiv);
 
-//Defining the searchPokemon App
+//Defining the searchPokemon App:
 const searchPokemon = async (pokemonId = 3) => {
   try {
     const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-    console.log(data)
     let pokeName = data.name;
     let pokeWeight = data.weight;
     let pokeHeight = data.height;
@@ -33,27 +31,66 @@ const searchPokemon = async (pokemonId = 3) => {
   }
 }
 
+// defining the get by type function:
+const PokemonByType = async (type) => {
+  try {
+    const sameTypeObject = await axios.get(`https://pokeapi.co/api/v2/type/${type}/`);
+    let sameTypeDataObject = sameTypeObject.data
+    let sameTypePoke = sameTypeDataObject.pokemon
+    let pokeNamesInList = []
+    sameTypePoke.forEach(element => {
+      pokeNamesInList.push(element.pokemon.name)
+    });
+    makeTypeList(pokeNamesInList);
+
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+// defining rthe function that creates the tye list
+const makeTypeList = (nameList) => {
+  let sameTypeList = document.getElementById("sameTypeList");
+  sameTypeList.innerText = "";
+  sameTypeList.innerText = "Pokemons with the same type:"
+  for (let x of nameList) {
+    const item = document.createElement('li');
+    item.className = "sameTypePoke";
+    sameTypeList.appendChild(item);
+    item.innerText += x
+  }
+}
+
+// defining the function that creates the div
 const makeDiv = (name, height, weight, types, frontSrc, backSrc) => {
 
   let typesArr = []
   types.forEach(element => {
     typesArr.push(element.type.name)
   });
-  pokeType = typesArr.join(", ")
+  pokeType = typesArr.join("</li> <li>")
 
   const htmlText = `
   <div class="pokemonContainer">
   <div>Name: ${name} </div> 
   <div>Height: ${height}</div>
   <div>Weight: ${weight}</div>
-  <div>Types: ${pokeType}</div>
-  <div>picture: <img id="pokeImg" src="${frontSrc}" </div>`;
+  <div> <ul> Types: <li> ${pokeType} </li> </ul> </div>
+  <div>picture: <img id="pokeImg" src="${frontSrc}" </div>
+  <ul id="sameTypeList"></ul>`;
   pokeDiv.innerHTML = htmlText;
 
+  // creating the flipping on hover
   let pokeImg = document.getElementById("pokeImg")
   pokeImg.addEventListener("mouseover", () => pokeImg.src = backSrc)
   pokeImg.addEventListener("mouseout", () => pokeImg.src = frontSrc)
+
+  //  defininig the press on item event
+  let typeItem = document.getElementsByTagName("li")
+  for (let x of typeItem) {
+    x.addEventListener("mousedown", () => PokemonByType(x.innerText))
+  }
 }
 
-
+// defining the click on search event
 searchButton.addEventListener("click", () => searchPokemon(input.value))
