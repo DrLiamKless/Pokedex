d712e29a717a9ca37dd4c7ca8f6fbfd60ca30419
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-08-05 22:51:43 +0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // Defining global values
 let searchButton = document.getElementById("searchButon");
 let input = document.getElementById("search");
@@ -5,74 +7,108 @@ let results = document.getElementById("results");
 let pokeDiv = document.createElement("div");
 results.appendChild(pokeDiv);
 
-//Defining the searchPokemon App:
+// Defining the searchPokemon App:
 const searchPokemon = async (pokemonId = 3) => {
   try {
-    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const { data } = await axios.get(
+      `http://pokeapi.co/api/v2/pokemon/${pokemonId}`
+    );
     let pokeName = data.name;
     let pokeWeight = data.weight;
     let pokeHeight = data.height;
-    let types = data.types
+    let types = data.types;
     let frontImgSrc = data.sprites.front_default;
     let backImgSrc = data.sprites.back_default;
 
     makeDiv(pokeName, pokeHeight, pokeWeight, types, frontImgSrc, backImgSrc);
     input.value = "";
     input.focus();
-  }
-  catch (error) {
-    console.log(error)
-    pokError = document.createElement("div")
-    pokError.className = "Error"
-    pokError.innerHTML = "Pokemon not found"
+  } catch (error) {
+    console.log(error);
+    pokError = document.createElement("div");
+    pokError.className = "Error";
+    pokError.innerHTML = "Pokemon not found";
     pokeDiv.appendChild(pokError);
     setTimeout(() => {
-      pokeDiv.innerHTML = ''
+      pokeDiv.innerHTML = "";
     }, 2000);
-
   }
-}
+};
+
+// const searchPokemon = async (pokemonId = 3) => {
+
+//   const options = {
+//     method: 'GET',
+//     mode: 'cors',
+//     cache: 'no-cache',
+//     credentials: 'same-origin',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   }
+//   await fetch(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`, options)
+//     .then(res => { res.json() })
+//     .then(data => {
+//       let pokeName = data.name;
+//       let pokeWeight = data.weight;
+//       let pokeHeight = data.height;
+//       let types = data.types
+//       let frontImgSrc = data.sprites.front_default;
+//       let backImgSrc = data.sprites.back_default;
+
+//       makeDiv(pokeName, pokeHeight, pokeWeight, types, frontImgSrc, backImgSrc);
+//       input.value = "";
+//       input.focus();
+//     })
+//   console.log(error)
+//   pokError = document.createElement("div")
+//   pokError.className = "Error"
+//   pokError.innerHTML = "Pokemon not found"
+//   pokeDiv.appendChild(pokError);
+//   setTimeout(() => {
+//     pokeDiv.innerHTML = ''
+//   }, 2000);
+// }
 
 // defining the get by type function:
 const PokemonByType = async (type) => {
   try {
-    const sameTypeObject = await axios.get(`https://pokeapi.co/api/v2/type/${type}/`);
-    let sameTypeDataObject = sameTypeObject.data
-    let sameTypePoke = sameTypeDataObject.pokemon
-    let pokeNamesInList = []
-    sameTypePoke.forEach(element => {
-      pokeNamesInList.push(element.pokemon.name)
+    const sameTypeObject = await axios.get(
+      `https://pokeapi.co/api/v2/type/${type}/`
+    );
+    let sameTypeDataObject = sameTypeObject.data;
+    let sameTypePoke = sameTypeDataObject.pokemon;
+    let pokeNamesInList = [];
+    sameTypePoke.forEach((element) => {
+      pokeNamesInList.push(element.pokemon.name);
     });
     makeTypeList(pokeNamesInList);
-
+  } catch (error) {
+    console.log(error);
   }
-  catch (error) {
-    console.log(error)
-  }
-}
+};
 // defining rthe function that creates the tye list
 const makeTypeList = (nameList) => {
   let sameTypeList = document.getElementById("sameTypeList");
   sameTypeList.innerText = "";
-  sameTypeList.innerText = "Pokemons with the same type:"
+  sameTypeList.innerText = "Pokemons with the same type:";
   for (let x of nameList) {
-    const item = document.createElement('li');
+    const item = document.createElement("li");
     item.className = "sameTypePoke";
     sameTypeList.appendChild(item);
-    item.innerText += x
+    item.innerText += x;
     // add the load new pokemon event
     item.addEventListener("click", () => searchPokemon(x));
   }
-}
+};
 
 // defining the function that creates the div
 const makeDiv = (name, height, weight, types, frontSrc, backSrc) => {
-
-  let typesArr = []
-  types.forEach(element => {
-    typesArr.push(element.type.name)
+  let typesArr = [];
+  types.forEach((element) => {
+    typesArr.push(element.type.name);
   });
-  pokeType = typesArr.join("</li> <li>")
+  pokeType = typesArr.join("</li> <li>");
 
   const htmlText = `
   <div class="pokemonContainer">
@@ -85,16 +121,16 @@ const makeDiv = (name, height, weight, types, frontSrc, backSrc) => {
   pokeDiv.innerHTML = htmlText;
 
   // creating the flipping on hover
-  let pokeImg = document.getElementById("pokeImg")
-  pokeImg.addEventListener("mouseover", () => pokeImg.src = backSrc)
-  pokeImg.addEventListener("mouseout", () => pokeImg.src = frontSrc)
+  let pokeImg = document.getElementById("pokeImg");
+  pokeImg.addEventListener("mouseover", () => (pokeImg.src = backSrc));
+  pokeImg.addEventListener("mouseout", () => (pokeImg.src = frontSrc));
 
   //  defininig the press on item event
-  let typeItem = document.getElementsByTagName("li")
+  let typeItem = document.getElementsByTagName("li");
   for (let x of typeItem) {
-    x.addEventListener("click", () => PokemonByType(x.innerText))
+    x.addEventListener("click", () => PokemonByType(x.innerText));
   }
-}
+};
 
 // defining the click on search event
-searchButton.addEventListener("click", () => searchPokemon(input.value))
+searchButton.addEventListener("click", () => searchPokemon(input.value));
